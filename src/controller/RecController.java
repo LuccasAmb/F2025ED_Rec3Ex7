0bea3cd47fff87e54d416db2e66d2647faf1dfef
LuccasAmb <ambrosio.luccas2002@gmail.com>
2025-02-19 16:12:07 -0300

    first

--- src/controller/RecController.java
+++ src/controller/RecController.java
@@ -6,31 +6,23 @@ public class RecController {
 		super();
 	}
 	
-	public int Rec(int numero, int cont) {
+	public int Rec(int numero, int digito, int cont) {
 		
-		String numeros[] = Integer.toString(numero).split("");
-		int tamanho = numeros.length;
-		
-		//A condição de parada é definida pelo contador sendo igual a tamanho do vetor de String com os dígitos do numero inteiro.
-		if(tamanho == cont) {
+		//Condição de parada definida quando o numero/10 for 0 é porque acabou os digitos daquele número
+		if(numero == 0) {
 			return cont;
-		}else {
-			//Contador aumenta para acumular na contagem e posteriormente comparar com o tamanho manualmente acessado pelo método length
-			cont++;
-			return Rec(numero, cont);
 		}
+		
+		if((numero%10) == digito) {
+			cont++;
+		}
+		
+		//vai checar usando o resto da divisao por 10 o último dígito se é igual ao dígito comparado digitado pelo usuario, se sim adiciona ao contador
+		//depois segue chamando a recursiva mas dividindo o numero por 10 para eliminar o ultimo digito e para o próximo poder ser checado.
+		return Rec(numero/10, digito, cont);
 		
 	}
 	
-	public int Rec2(int numero) {
-		
-		//Condição de parada é quando sobra apenas um dígito
-		if(numero < 10) {
-			return 1;
-		}
-		
-		//Dividir por 10 o número original, reduzindo um dígito dele, caso ele seja maior ou igual a 10, ou seja, tenha mais de um dígito.
-		return 1 + Rec2(numero/10);
-	}
+
 	
 }
--- src/view/Rec.java
+++ src/view/Rec.java
@@ -8,11 +8,18 @@ public class Rec {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int numero;
+		int numero, digito;
 		RecController rec = new RecController();
-		numero = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um número inteiro"));
-		System.out.println(rec.Rec(numero, 0));
-		System.out.println(rec.Rec2(numero));
+		do {
+			numero = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um número inteiro de 10 a 999999"));
+		}while(numero < 10 && numero > 999999);
+		
+		do {
+			digito = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um número de 0 a 9"));
+		}while(digito < 0 && digito > 9);
+		
+		
+		System.out.println(rec.Rec(numero, digito, 0));
 	}
 
 }

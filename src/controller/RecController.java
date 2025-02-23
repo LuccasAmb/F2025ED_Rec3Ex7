f577aaa8288b9efa79f38563ef1876f97807e966
LuccasAmb <ambrosio.luccas2002@gmail.com>
2025-02-23 19:29:05 -0300

    fixes

--- src/controller/RecController.java
+++ src/controller/RecController.java
@@ -8,9 +8,11 @@ public class RecController {
 	
 	public String Rec(String string, String gnirts) {
 		
+		//condição de parada: quando a subtring retornar um string vazia depois de acessar todos os ultimos caracteres
 		if(string == "") {
 			return gnirts;
 		}else {
+			//pega o termo atual e envia ele menos o ultimo caractere (que já foi adicionado na string ao contrario)
 			gnirts+= string.substring(string.length()-1);
 			return Rec(string.substring(0, (string.length() - 1)), gnirts);
 		}

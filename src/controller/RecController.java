f3b8035940e28871a42022cb94f5747ba645e6d1
LuccasAmb <ambrosio.luccas2002@gmail.com>
2025-02-23 21:08:36 -0300

    first

--- src/controller/RecController.java
+++ src/controller/RecController.java
@@ -6,17 +6,19 @@ public class RecController {
 		super();
 	}
 
-
-	
 	public int Rec(int n) {
-		
-		if(n == 0) {
+
+		// n/10 para acessar o numero menos o ultimo digito, quando n=0 é porque acabou
+		// os digitos, dai encerra retornando 0 na soma.
+		if (n == 0) {
 			return 0;
-		}else {
-			int soma = (n%10);
-			return soma + Rec(n/10);
+		} else {
+			// retorna o ultimo digito acessado pelo resto na divisão por 10 + a propria
+			// recursiva enviando o numero original menos o ultimo digtio para acessar o
+			// penultimo e assim por diante
+			return n % 10 + Rec(n / 10);
 		}
-		
+
 	}
 
 }
